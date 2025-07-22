b15dfb135f7d1282950d67494ad774da5b56ce98
Prudence Levison <you@example.com>
2025-07-22 15:03:51 +0100

    worked on the search bar feature

--- src/app/Components/SearchBar.jsx
+++ src/app/Components/SearchBar.jsx
@@ -1,13 +1,30 @@
+"use client"
 import React from "react";
+import { useState } from 'react'
 
 const SearchBar = () => {
+  const[enterlocation, setEnterLocation] =useState("");
+
+  const handleInputButton =(e)=>(
+   setEnterLocation(e.target.value)
+  )
+
+  const submitHandler =(e) =>{
+    e.preventDefault();
+    console.log("submitted", enterlocation);
+    setEnterLocation('')
+  }
+
   return (
     <div className="w-full py-6">
-      <input
+      <form onSubmit={submitHandler}>
+      <input onChange={handleInputButton}
         className="w-full px-5 py-3 rounded-full shadow-lg bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-cyan-400"
         placeholder="Search for ATM's near you"
         type="text"
+        value={enterlocation}
       />
+      </form>
     </div>
   );
 };
